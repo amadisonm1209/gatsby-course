e9e50f6bf7252a8ca6ff002d799b2bf25c8abef7
Mads <madisonmartinez@Alicias-MacBook-Pro.local>
2020-11-10 14:10:09 -0600

    fixed nav responsiveness

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -42,8 +42,27 @@ const NavStyles = styled.nav`
             &:hover {
                 color: var(--red);
             }
+            @media (max-width:800px) {
+                font-size: 2rem;
+            }
             &[aria-current="page"] {
                 color: var(--red);
+            }
+        }
+        @media(max-width: 600px) {
+            --columns: 4;
+            margin-bottom: 2rem;
+            ul {
+                grid-template-rows: auto auto;
+                grid-template-columns: repeat(var(--columns), 1fr);
+                justify-items: center;
+            }
+            .logo-item {
+                order: 0;
+                grid-column: 1 / -1;
+            }
+            .logo {
+                transform: none;
             }
         }
    
@@ -59,7 +78,7 @@ export default function Nav() {
                 <li>
                     <Link to="/pizzas">Pizza Menu</Link>
                 </li>
-                <li>
+                <li className="logo-item">
                     <Link to="/"> <Logo /> </Link>
                 </li>
                 <li>
